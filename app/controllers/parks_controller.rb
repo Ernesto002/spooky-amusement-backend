856e6776cd7314d333235f929477839f911ca13d
ernesto <ernesto002@icloud.com>
2021-09-26 11:46:01 -0700

    seed db with park objects/jsonify objects

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -1,2 +1,7 @@
 class ParksController < ApplicationController
+
+    def index
+        parks = Park.all
+        render json: parks
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Park.create(name: "Paranormal Pines", address: Faker::Address.street_address, city: Faker::Address.city, state: "Nevada", detail: "A park flooded with ghostly phenomenon.", image_url: "https://tripswithtykes.com/wp-content/uploads/2021/09/Disneyland-Halloween-Time-Mickey-Pumpkin-at-Night.jpg")
+Park.create(name: "Massacre Maze", address: Faker::Address.street_address, city: Faker::Address.city, state: "California", detail: "A mysterious maze with a dark past.", image_url: "https://modernfarmer.com/wp-content/uploads/2015/10/rsz_shutterstock_156226406-1200x667.jpg")
+Park.create(name: "Corpse Castle", address: Faker::Address.street_address, city: Faker::Address.city, state: "Ohio", detail: "A chilling medival castle with hidden secrets.", image_url: "https://wallpaperaccess.com/full/666549.jpg")
