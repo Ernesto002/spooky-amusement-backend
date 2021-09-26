4582838b9dd22221ef75fa104e72d944c342c427
ernesto <ernesto002@icloud.com>
2021-09-26 12:56:34 -0700

    revise seeding image

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Park.create(name: "Paranormal Pines", address: Faker::Address.street_address, city: Faker::Address.city, state: "Nevada", detail: "A park flooded with ghostly phenomenon.", image_url: "https://tripswithtykes.com/wp-content/uploads/2021/09/Disneyland-Halloween-Time-Mickey-Pumpkin-at-Night.jpg")
+Park.create(name: "Paranormal Pines", address: Faker::Address.street_address, city: Faker::Address.city, state: "Nevada", detail: "A park flooded with ghostly phenomenon.", image_url: "https://images.unsplash.com/photo-1536850428371-f12bc9f3159a?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y3JlZXB5JTIwZm9yZXN0fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80")
 Park.create(name: "Massacre Maze", address: Faker::Address.street_address, city: Faker::Address.city, state: "California", detail: "A mysterious maze with a dark past.", image_url: "https://modernfarmer.com/wp-content/uploads/2015/10/rsz_shutterstock_156226406-1200x667.jpg")
 Park.create(name: "Corpse Castle", address: Faker::Address.street_address, city: Faker::Address.city, state: "Ohio", detail: "A chilling medival castle with hidden secrets.", image_url: "https://wallpaperaccess.com/full/666549.jpg")
