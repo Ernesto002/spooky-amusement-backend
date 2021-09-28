ffd5286c2cd698d3972d228b769e998f968b3b1f
ernesto <ernesto002@icloud.com>
2021-09-27 19:26:47 -0700

    add park params for park creation

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -4,4 +4,15 @@ class ParksController < ApplicationController
         parks = Park.all
         render json: parks
     end
+
+    def create
+        park = Park.create(park_params)
+        render json: park
+    end
+
+    private 
+
+    def park_params
+        params.require(:park).permit(:name, :address, :city, :state, :detail, :image_url)
+    end
 end
