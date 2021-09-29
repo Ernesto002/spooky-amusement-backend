1dcbb2aea4925a213fa623c16e366cc4eeea294a
ernesto <ernesto002@icloud.com>
2021-09-29 12:25:01 -0700

    add user model/add user to park

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_28_183011) do
+ActiveRecord::Schema.define(version: 2021_09_29_192153) do
 
   create_table "attractions", force: :cascade do |t|
     t.string "title"
@@ -30,6 +30,12 @@ ActiveRecord::Schema.define(version: 2021_09_28_183011) do
     t.string "state"
     t.string "detail"
     t.string "image_url"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
